6f5adb0bc79c331b7044f2834a1d6e5396a42557
Yasmine Halim <Yas@Yasmines-MacBook-Air.local>
2018-04-07 12:08:28 -0400

    friendfinder hw

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'Donald Trump',
+		photo: 'http://theresurgent.com/wp-content/uploads/2016/02/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1.jpg',
+		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+	},{
+		name: 'Alex Morgan',
+		photo: 'http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg',
+		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+	},{
+		name: 'Phil Dunphy',
+		photo: 'https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'Natalie Coughlin',
+		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/natalie-olympics.jpg',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
---
+++ app/routing/api-routes.js
@@ -0,0 +1,12 @@
+var friendList = require('../data/friends.js');
+var path = require('path');
+
+module.exports = function(app){
+	app.get('/api/friends', function(req, res){
+		res.json(friendList);
+	});
+
+	app.post('/api/friends', function(req, res){
+		friendList.push(req.body);
+	})
+};
